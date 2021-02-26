580627e380655918b5f6389e54eb9a9ef3f432b3
Leonardo <Leoeyeschmittk@gmail.com>
2021-02-25 22:37:27 -0300

    Adding the events and the  styling. Making executable

---
+++ index.js
@@ -0,0 +1,42 @@
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+const ratingResult = document.querySelector(".rating__result");
+
+printRatingResult(ratingResult);
+
+function executeRating(stars, result) {
+   const starClassActive = "rating__star fas fa-star";
+   const starClassUnactive = "rating__star far fa-star";
+   const starsLength = stars.length;
+
+   stars.map((star) => {
+      star.onclick = () => {
+         i = stars.indexOf(star);
+
+         if (star.className.indexOf(starClassUnactive) !== -1) {
+            printRatingResult(result, i + 1);
+            for (i; i >= 0; --i) stars[i].className = starClassActive;
+         } else {
+            printRatingResult(result, i);
+            for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+         }
+      };
+
+      star.onmouseover = () => {
+         i = stars.indexOf(star);
+         if (star.className === starClassUnactive) {
+            for (i; i >= 0; --i) stars[i].classList.add("active");
+         }
+      };
+
+      star.onmouseout = () => {
+         i = stars.indexOf(star);
+         for (i; i >= 0; --i) stars[i].classList.remove("active");
+      };
+   });
+}
+
+function printRatingResult(result, num = 0) {
+   result.textContent = `${num}/5`;
+}
+
+executeRating(ratingStars, ratingResult);
